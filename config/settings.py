d4d1fb6987f79cab22e8314379186a3aa222e62b
FDSoftware <diego+gh@churrosoft.ar>
2025-09-18 01:48:21 -0300

    Add Django REST, ADRF, and Celery; update settings and dependencies

--- config/settings.py
+++ config/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "adrf",
     "faucet",
 ]
 
@@ -115,5 +117,17 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
+    ],
+}
+
+# Celery Configuration
+CELERY_TIMEZONE = "America/Buenos_Aires"
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
 
 init_web3()
